fa77746e6532396551e8e5282cf5ebc8ec513e5c
HoTranThienNhan <nhanhotranthien@gmail.com>
2023-08-30 15:03:17 +0700

    mongodb-connection-version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const { default: mongoose } = require("mongoose")
 dotenv.config()
 
 const app = express()
@@ -7,6 +8,14 @@ const port = process.env.PORT || 3001 // different from port 3000 of frontend
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+mongoose.connect(`mongodb+srv://nhanhotranthien:${process.env.MONGO_DB}@cluster0.7bu2zz1.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    console.log('Connect MongoDB successful!')
+})
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(port, () => {
