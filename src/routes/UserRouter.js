f5556f3fdb9cf5fe079093e8f759fe8c8b61a32b
HoTranThienNhan <nhanhotranthien@gmail.com>
2023-09-23 18:52:30 +0700

    admin-privilege-version

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const ProductController = require('../controllers/ProductController');
 const { authAdminMiddleware, authUserMiddleware } = require("../middleware/authMiddleware");
 
+// user routes
 router.post('/signup', UserController.createUser);
 router.post('/signin', UserController.signinUser);
 router.post('/signout', UserController.signoutUser);
@@ -11,6 +13,10 @@ router.delete('/deleteUser/:id', authAdminMiddleware, UserController.deleteUser)
 router.get('/getAllUsers', authAdminMiddleware, UserController.getAllUsers);
 router.get('/getUserDetails/:id', authUserMiddleware, UserController.getUserDetails);
 
+// product routes
+router.get('/getAllProducts', authAdminMiddleware, ProductController.getAllProducts);
+
+// token routes
 router.post('/refreshToken', UserController.refreshToken);
 
 module.exports = router
