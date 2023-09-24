69306900a81226949deee1a11cc2b5bef3f295ba
HoTranThienNhan <nhanhotranthien@gmail.com>
2023-09-24 22:57:56 +0700

    update_product_version

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -4,8 +4,8 @@ const ProductController = require('../controllers/ProductController');
 const { authAdminMiddleware } = require("../middleware/authMiddleware");
 
 router.post('/createProduct', ProductController.createProduct);
-router.delete('/deleteProduct/:id', authAdminMiddleware, ProductController.deleteProduct);
-router.put('/updateProduct/:id', authAdminMiddleware, ProductController.updateProduct);
+router.delete('/deleteProduct/:id', ProductController.deleteProduct);
+router.put('/updateProduct/:id', ProductController.updateProduct);
 router.get('/getProductDetails/:id', ProductController.getProductDetails);
 router.get('/getAllProducts', ProductController.getAllProducts);
 
